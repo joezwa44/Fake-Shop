1a82510557072062fa4802cb17ab10de3e6e3d0b
joezwa44 <114447010+joezwa44@users.noreply.github.com>
2023-01-15 19:47:30 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+         <Routes>
+          <Route path= {"/"} element = { <ProductListing/> } />
+          <Route path={"/product/:productId"} element = { <ProductDetail/> } />
+          <Route>404 not found!</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
